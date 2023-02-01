770ac2e8d527476f8dd1463ef08311fbbe9f6fa2
phong <darkmirror2000@gmail.com>
2023-02-01 16:52:53 +0700

    new change

--- WorkBook/Assgn27.java
+++ WorkBook/Assgn27.java
@@ -1,5 +1,5 @@
 package WorkBook;
-public static class section {
+public class Assgn27 {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("no");
